fea94bc4a7f6d0f7ee6f69d8d5110439bab340b7
cullivanben <benjc.coding@gmail.com>
2021-03-21 23:50:23 -0400

    Dinic

--- graph/Dinic.cc
+++ graph/Dinic.cc
@@ -4,31 +4,35 @@
     Source: https://cp-algorithms.com/graph/dinic.html
 */
 struct Dinic {
-    vector<vector<int>> adj;
-    vector<pair<int, int>> cut;
-    vector<int> to;
-    vector<long long> cap;
-    vector<int> level;
-    vector<int> ptr;
-    queue<int> q;
-    int source;
-    int sink;
-    long long mf = 0;
-    const long long MAX_CAP = 5e18;
+    vector<vector<int>> adj; // adj[i] is the list of indices of the out-edges of the ith vertex
+    vector<pair<int, int>> cut; // the edges that form the min cut
+    vector<int> to; // to[i] is the vertex that the ith edge points to
+    vector<long long> cap; // cap[i] is the capacity of the ith edge
+    vector<int> level; // level[i] is the level of the ith vertex
+    vector<int> ptr; // ptr[i] points to the next unvisited neighbor of the ith vertex
+    queue<int> q; // for bfs
+    int source; // the source vertex
+    int sink; // the sink vertex
+    long long mf = 0; // the value of the max flow
+    const long long MAX_CAP = 5e18; // the maximum edge capacity
 
+    // constructs a redisual network with 0 edges
     Dinic(int _n) : adj(_n), level(_n), ptr(_n) {}
 
+    // allocates space for _m/2 edges
     inline void reserve(int _m) {
         to.reserve(_m);
         cap.reserve(_m);
     }
 
+    // initializes the residual network with the provided list of edges
     void init(vector<array<long long, 3>>& edg) {
         for (array<long long, 3>& v:edg) {
             add_edge(v[0], v[1], v[2]);
         }
     }
 
+    // adds the edge with the given capacity and the reverse edge to the residual network
     inline void add_edge(int u, int v, long long c) {
         to.push_back(v);
         cap.push_back(c);
@@ -38,6 +42,8 @@ struct Dinic {
         adj[v].push_back(cap.size() - 1);
     }
 
+    // finds the level of each vertex, beginning from the source
+    // returns whether the sink is reachable from the source
     bool bfs() {
         fill(level.begin(), level.end(), -1);
         level[source] = 0;
@@ -55,6 +61,7 @@ struct Dinic {
         return level[sink] != -1;
     }
 
+    // attempts to push 'push_flow' units of flow from vertex u toward the sink i.e. up a level
     long long dfs(int u, long long push_flow) {
         if (u == sink || push_flow == 0) {
             return push_flow;
@@ -73,6 +80,7 @@ struct Dinic {
         return 0;
     }
 
+    // finds the max flow from s to t
     long long max_flow(int s, int t) {
         source = s;
         sink = t;
@@ -90,6 +98,7 @@ struct Dinic {
         return mf;
     }
 
+    // finds the min st cut
     void min_cut() {
         bfs();
         for (int u = 0; u < (int) level.size(); u++) {
