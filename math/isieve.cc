2ff3afcaa4e928ab2b51bcc0c362442240ca2da5
cullivanben <benjc.coding@gmail.com>
2021-03-22 15:24:09 -0400

    math

---
+++ math/isieve.cc
@@ -0,0 +1,21 @@
+/*
+    The sieve of Eratosthenes.
+    Time: O(N)
+    Source: https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+*/
+vector<int> primes;
+void sieve(int N) {
+    vector<bool> is_prime(N + 1, true);
+    vector<int> smallest_prime_factor(N + 1);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= N; i++) {
+        if (is_prime[i]) {
+            smallest_prime_factor[i] = i;
+            primes.push_back(i);
+        }
+        for (int j = 0; j < (int) primes.size() && primes[j] <= smallest_prime_factor[i] && (long long) i * primes[j] <= N; j++) {
+            is_prime[i * primes[j]] = false;
+            smallest_prime_factor[i * primes[j]] = primes[j];
+        }
+    }
+}
---
+++ math/lsieve.cc
@@ -0,0 +1,21 @@
+/*
+    The sieve of Eratosthenes.
+    Time: O(N)
+    Source: https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+*/
+vector<long long> primes;
+void sieve(long long N) {
+    vector<bool> is_prime(N + 1, true);
+    vector<long long> smallest_prime_factor(N + 1);
+    is_prime[0] = is_prime[1] = false;
+    for (long long i = 2; i <= N; i++) {
+        if (is_prime[i]) {
+            smallest_prime_factor[i] = i;
+            primes.push_back(i);
+        }
+        for (long long j = 0; j < (long long) primes.size() && primes[j] <= smallest_prime_factor[i] && i * primes[j] <= N; j++) {
+            is_prime[i * primes[j]] = false;
+            smallest_prime_factor[i * primes[j]] = primes[j];
+        }
+    }
+}
