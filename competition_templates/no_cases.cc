2611998bad3037bb89ccd2be6bd4c0c1a0454e6c
cullivanben <benjc.coding@gmail.com>
2021-03-21 22:21:15 -0400

    templates

---
+++ competition_templates/no_cases.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#ifdef IL_MIO_PORTATILE
+#include debug_path
+#else
+#define dbg(...) 101
+#endif
+const char df = '\n';
+void solve() {
+    
+}
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    //int tt; cin >> tt; while (tt--)
+    solve();
+    return 0;
+}
