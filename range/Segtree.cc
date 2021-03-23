83e6cbc9c356427b30ddc67f5fdc1f398b77b890
cullivanben <benjc.coding@gmail.com>
2021-03-23 00:12:00 -0400

    Lazy

--- range/Segtree.cc
+++ range/Segtree.cc
@@ -18,47 +18,54 @@ struct Segtree {
         build_Segtree(1, 0, len - 1, input);
     }
 
+    // builds the Segtree using the nodes in the input array
     void build_Segtree(int i, int tl, int tr, vector<Node>& input) {
         if (tl == tr) {
+            // the current node is a leaf
             data[i] = input[tl];
             return;
         }
+        // this node is not a leaf -> recurse on children
         int tm = (tl + tr) >> 1;
         build_Segtree(i << 1, tl, tm, input);
         build_Segtree(i << 1 | 1, tm + 1, tr, input);
+        // set the value of this node by combining the values of its children
         data[i] = combine(data[i << 1], data[i << 1 | 1]);
     }
 
     // sets the element at index x to v
-    void upd(int i, int tl, int tr, int x, Node v) {
+    void upd(int i, int tl, int tr, int x, const Node& v) {
         if (tl == x && tr == x) {
-            data[i] = v;
+            data[i] = v; // we are at x
             return;
         }
+        // we are not at x that is being updated -> recurse on the child that has x as a leaf
         int tm = (tl + tr) >> 1;
         if (x <= tm) {
             upd(i << 1, tl, tm, x, v);
         } else {
             upd(i << 1 | 1, tm + 1, tr, x, v);
         }
+        // update the current node
         data[i] = combine(data[i << 1], data[i << 1 | 1]);
     }
 
     // sets the element at index x to v
-    void upd(int x, Node v) {
+    void upd(int x, const Node& v) {
         upd(1, 0, len - 1, x, v);
     }
 
     // queries the segment [l, r]
     Node get(int i, int tl, int tr, int l, int r) {
-        if (l > r) {
-            return Node();
-        } else if (tl == l && tr == r) {
-            return data[i];
+        if (tr < l || tl > r) {
+            return Node(); // we are out of range -> return the default value
         }
+        if (tl >= l && tr <= r) {
+            return data[i]; // this node's entire segment is in the range 
+        }
+        // we are partially in the range -> recurse on children
         int tm = (tl + tr) >> 1;
-        return combine(get(i << 1, tl, tm, l, min(tm, r)), 
-                       get(i << 1 | 1, tm + 1, tr, max(tm + 1, l), r));
+        return combine(get(i << 1, tl, tm, l, r), get(i << 1 | 1, tm + 1, tr, l, r));
     }
 
     // queries the segment [l, r]
@@ -69,13 +76,10 @@ struct Segtree {
 // --------------------------------------
     // CHANGE
     struct Node {
-
-        Node() {
-            // init default values
-        }
+        ll v = 0;
     };
     Node combine(const Node& a, const Node& b) {
-
+        return {a.v + b.v};
     }
 // --------------------------------------
 }; // Segtree
