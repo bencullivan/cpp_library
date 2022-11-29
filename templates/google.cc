f852a95069f721146fff0eb44daa9ef1fae570ee
bencullivan <benjc.coding@gmail.com>
2022-11-28 19:47:12 -0500

    remove com

--- templates/google.cc
+++ templates/google.cc
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
-#define sz(v) int64_t(v.size())
 using namespace std;
 using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vi=vector<int>;
+using vl=vector<ll>;
+#define rep(i,a,b) for(int i=a;i<(b);++i)
+#define all(v) v.begin(),v.end()
+#define sz(v) int64_t(v.size())
 
 void solve_one() {
 }
-
 int main() {
-    ios_base::sync_with_stdio(0), cin.tie(0);
-    int t;
-    cin >> t;
-    for (int i = 1; i <= t; i++) {
-        cout << "Case #" << i << ": ";
-        solve_one();
-    }
+  ios_base::sync_with_stdio(0), cin.tie(0);
+  int t;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    cout << "Case #" << i << ": ";
+    solve_one();
+  }
 }
--- templates/template.cc
+++ templates/template.cc
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vi=vector<int>;
+using vl=vector<ll>;
+#define rep(i,a,b) for(int i=a;i<(b);++i)
+#define all(v) v.begin(),v.end()
+#define sz(v) int64_t(v.size())
 
 void solve_one() {
 }
