56fc04252a7dac9512c6529424a880c552037ce9
bencullivan <benjc.coding@gmail.com>
2022-12-02 03:30:56 -0500

    macros

--- templates/template.cc
+++ templates/template.cc
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
-using namespace std;
-using ll=long long;
 #define f first
 #define s second
-#define pb push_back
-#define all(v) v.begin(),v.end()
-#define sz(v) int64_t(v.size())
+using ll=long long;
+using namespace std;
 
 void solve_one() {
 }
--- templates/template_no_cases.cc
+++ templates/template_no_cases.cc
@@ -1,11 +1,8 @@
 #include <bits/stdc++.h>
-using namespace std;
-using ll=long long;
 #define f first
 #define s second
-#define pb push_back
-#define all(v) v.begin(),v.end()
-#define sz(v) int64_t(v.size())
+using ll=long long;
+using namespace std;
 
 signed main() {
   ios_base::sync_with_stdio(0), cin.tie(0);
