1dc9680227469503b4d5a928d3c985e5cc86fdd4
bencullivan <benjc.coding@gmail.com>
2023-03-22 21:53:07 -0400

    namespace std

--- range/range_sum.cc
+++ range/range_sum.cc
@@ -1,7 +1,7 @@
 // get sum over the range [l, r]
-template <typename T> T range_sum(const vector<T> &v, int l, int r) {
-	r = min(r, (int)v.size() - 1);
-	l = max(l, 0);
+template <typename T> T range_sum(const std::vector<T> &v, int l, int r) {
+	r = std::min(r, (int)v.size() - 1);
+	l = std::max(l, 0);
 	if (r < l) return 0;
 	T ans = v[r];
 	if (l) ans -= v[l - 1];
